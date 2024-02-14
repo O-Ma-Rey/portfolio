4c8525408b14602f6e03058e840e10e0f1e7e40c
Owen Reyes <OM.Reyes2609@gmail.com>
2024-02-14 16:03:14 +0000

    Added some animations and toggle display mode

--- script.js
+++ script.js
@@ -1,40 +1,64 @@
 // Variables
+const nav = document.querySelector('#nav');
+const topOfNav = nav.offsetTop;
+
+const toggle = document.querySelector('.checkbox');
 const exploreBtn = document.querySelector('.explore');
-const aboutBtn = document.querySelector('.panel-about');
-const resumeBtn = document.querySelector('.panel-resume');
-const projectBtn = document.querySelector('.panel-projects');
-const contactBtn = document.querySelector('.panel-contact');
+
+const aboutBtn = document.querySelector('.nav-about');
+const resumeBtn = document.querySelector('.nav-resume');
+const projectBtn = document.querySelector('.nav-project');
+const contactBtn = document.querySelector('.nav-contact');
 
 // Functions
+function fixNav() {
+  window.addEventListener('scroll', fixNav);
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+function toggleLight() {
+  var elem = document.body;
+  elem.classList.toggle("light-mode");
+}
+
 function toNav() {
-  var elem = document.getElementById('nav');
-  elem.scrollIntoView();
+  const elem = document.getElementById('nav');
+  elem.scrollIntoView({behavior: "smooth"});
 }
 
 function toAbout() {
-  var elem = document.getElementById('about');
-  elem.scrollIntoView();
+  const elem = document.getElementById('about');
+  elem.scrollIntoView({behavior: "smooth"});
 }
 
 function toResume() {
-  var elem = document.getElementById('resume');
-  elem.scrollIntoView();
+  const elem = document.getElementById('resume');
+  elem.scrollIntoView({behavior: "smooth"});
 }
 
-function toProjects() {
-  var elem = document.getElementById('projects');
-  elem.scrollIntoView();
+function toProject() {
+  const elem = document.getElementById('projects');
+  elem.scrollIntoView({behavior: "smooth"});
 }
 
 function toContact() {
-  var elem = document.getElementById('contact');
-  elem.scrollIntoView();
+  const elem = document.getElementById('contact');
+  elem.scrollIntoView({behavior: "smooth"});
 }
 
-// Event Listeners
+// Event listeners
+window.addEventListener('scroll', fixNav);
+
+toggle.addEventListener('click', toggleLight);
 exploreBtn.addEventListener('click', toNav);
+
 aboutBtn.addEventListener('click', toAbout);
 resumeBtn.addEventListener('click', toResume);
-projectBtn.addEventListener('click', toProjects);
+projectBtn.addEventListener('click', toProject);
 contactBtn.addEventListener('click', toContact);
-
